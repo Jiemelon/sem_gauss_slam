b3f4047a305fe4ba7573e8d47922525a059ac474
Jiemelon <113030236+Jiemelon@users.noreply.github.com>
2025-09-18 10:21:17 +0800

    Update scannet.py

--- configs/scannet/scannet.py
+++ configs/scannet/scannet.py
@@ -43,7 +43,7 @@ config = dict(
     model=dict(
         c_dim=16,    # feature dimension
         pretrained_model_path=f"/data0/3dg/splatam/segmentation/scannet/dinov2_{scene_name}.pth",
-        n_classes=28, # number of nlasses (需要修改，这个是room0的)
+        n_classes=28, # number of nlasses 
         # 相机的参数
         crop_edge=10,
         H=480,
